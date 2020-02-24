fec64fc52773b1c93cbf3d162b16db61fad07294
yurenwang <sywyr2013@gmail.com>
2020-02-23 23:38:43 -0500

    End of Section 10 - Edit profile page

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
 import { User } from '../_models/user';
 import { UserService } from '../_services/user.service';
@@ -8,7 +8,7 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
-    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
+    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) { }
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
         return this.userService.getUsers().pipe(
@@ -17,6 +17,6 @@ export class MemberListResolver implements Resolve<User[]> {
                 this.router.navigate(['/home']);
                 return of(null);
             })
-        )
+        );
     }
 }
