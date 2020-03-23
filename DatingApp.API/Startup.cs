9c7b9a319c6dd6b75931448bda615c24f47863d4
frederickwang92 <sywyr2013@gmail.com>
2020-03-23 00:38:19 -0400

    Configure angular AOT prod build

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -92,9 +92,13 @@ namespace DatingApp.API
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
